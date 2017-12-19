89d2d15c08812860e94d213bd5d11a7ced520e2d
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-19 03:09:16 -0200

    Removido codigo em PHP para arquivos da pasta handlers.

--- register.php
+++ register.php
@@ -1,46 +1,5 @@
 <?php
-
-function sanitizeFormPassword($inputText) {
-    $inputText = strip_tags($inputText);
-    return $inputText;
-}
-
-function sanitizeFormUsername($inputText) {
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    return $inputText;
-}
-
-function sanitizeFormString($inputText) {    
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    $inputText = ucfirst(strtolower($inputText));
-    return $inputText;
-}
-
-if(isset($_POST['loginButton'])) {
-    //login button was pressed
-
-}
-
-if(isset($_POST['registerButton'])) {
-    //register was pressed
-    $username  = sanitizeFormUsername($_POST['username']);
-    
-    $firstName = sanitizeFormString($_POST['firstName']);
-
-    $lastName  = sanitizeFormString($_POST['lastName']);
-
-    $email     = sanitizeFormString($_POST['email']);
-
-    $email2    = sanitizeFormString($_POST['email']);
-
-    $password  = sanitizeFormPassword($_POST['password']);
-    
-
-}
-
-
+    include("includes/handlers/register-handler.php");
 ?>
 
 
