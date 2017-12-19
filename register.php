618153e6424a5320a61e1948e3e31e19eaf87b1f
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-19 02:46:41 -0200

    Tratamento nos formularios. Funções sanitizeFormPassword,  sanitizeFormUsername, sanitizeFormString.

--- register.php
+++ register.php
@@ -1,11 +1,43 @@
 <?php
 
-if (isset($_POST['loginButton'])) {
-    //echo "login button was pressed";
+function sanitizeFormPassword($inputText) {
+    $inputText = strip_tags($inputText);
+    return $inputText;
 }
 
-if (isset($_POST['registerButton'])) {
-    echo "register was pressed";
+function sanitizeFormUsername($inputText) {
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    return $inputText;
+}
+
+function sanitizeFormString($inputText) {    
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+
+if(isset($_POST['loginButton'])) {
+    //login button was pressed
+
+}
+
+if(isset($_POST['registerButton'])) {
+    //register was pressed
+    $username  = sanitizeFormUsername($_POST['username']);
+    
+    $firstName = sanitizeFormString($_POST['firstName']);
+
+    $lastName  = sanitizeFormString($_POST['lastName']);
+
+    $email     = sanitizeFormString($_POST['email']);
+
+    $email2    = sanitizeFormString($_POST['email']);
+
+    $password  = sanitizeFormPassword($_POST['password']);
+    
+
 }
 
 
@@ -40,8 +72,8 @@ if (isset($_POST['registerButton'])) {
             <form id="registerForm" action="register.php" method="POST">
                 <h2>Create your free account</h2>
                 <p>
-                <label for="Username">Username</label>
-                <input id="Username" name="Username" type="text" placeholder"e.g. bartSimpson" require>
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" placeholder"e.g. bartSimpson" require>
                 <p>
 
                 <p>
